66b9f4376136e53ff9aedea96867ba441f30505f
leejh <ssleejh94@naver.com>
2018-05-10 14:17:31 +0900

    modify mic python file

--- Razberry/transcribe_streaming_mic.py
+++ Razberry/transcribe_streaming_mic.py
@@ -167,56 +167,19 @@ def listen_print_loop(responses):
 
             # Exit recognition if any of the transcribed phrases could be
             # one of our keywords.
-            if re.search(r'\b(그만)\b', transcript+overwrite_chars, re.I):
-                print('Exiting..')
-                break
-            
-            #if transcript.encode('utf-8') == "사랑해" or " 사랑해":
-            #    print("???") 
+            #if re.search(r'\b(그만)\b', transcript+overwrite_chars, re.I):
+            #    print('Exiting..')
             #    break
+            
+            if transcript.encode('utf-8') == "종료" or transcript.encode('utf-8') == " 종료":
+                print("finishing...")
+                #break
 
             num_chars_printed = 0
-
-	    if transcript.encode('utf-8') == "사진" or " 사진":
-                 os.system("python myface.py")
-
-                 '''
-	   	 #initialize the camera and grab a reference to the raw camera capture
-	    	 camera = PiCamera()
-	   	 camera.resolution = (640, 480)
-	   	 camera.framerate = 32
-	   	 rawCapture = PiRGBArray(camera, size=(640, 480))
-
-	   	 cascade = cv2.CascadeClassifier("/home/pi/opencv-3.3.0/data/haarcascades/haarcascade_frontalface_alt.xml")
-
-		#allow the camera to warmup
-	   	 time.sleep(0.1)
-
-		#capture frames from the camera
-	   	 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    			#grab the raw Numpy array representing the image, then initialize the timestamp
-    			#and occupied/unoccupied text
-    			img = frame.array
-
-			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    			gray = cv2.equalizeHist(gray)
-
-			rects = myface,detect(gray, cascade)
-			vis = img.copy()
-			myface,draw_rects(vis, rects, (0, 255, 0))
-    
-    			#show the frame
-    			cv2.imshow("Frame", vis)
-    			key = cv2.waitKey(1) & 0xFF
-
-    			#clear the stream in preparation for the next frame
-    			rawCapture.truncate(0)
-
-    			#if the 'q' key was pressed, break from the loop
-			if  key == ord("q"):
-        			break
-                        '''
-                    
+            
+            if transcript.encode('utf-8') == "사진" or transcript.encode('utf-8') == " 사진":
+                print("picture")
+   
 
 def main():
     # See http://g.co/cloud/speech/docs/languages
